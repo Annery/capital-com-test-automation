6a50f8e7d3f3f2531b27519f4d92cf35e50851c1
Annery <victoriabobrovskaya@gmail.com>
2026-05-22 14:57:57 +0300

    - Refactored header tab locators

--- components/Header.ts
+++ components/Header.ts
@@ -5,8 +5,8 @@ type SiteLocale = {
         personal: string;
         professional: string;
         support: string;
-    }
-}
+    };
+};
 
 export class Header {
     readonly root: Locator;
@@ -17,16 +17,14 @@ export class Header {
     constructor(page: Page, siteLocale: SiteLocale) {
         this.root = page.getByTestId('header');
 
-        this.personalTab = this.root
-            .locator(`a.GQwn[href="${siteLocale.routes.personal}"]`)
-            .filter({ visible: true });
+        this.personalTab = this.getVisibleHeaderTabByHref(siteLocale.routes.personal);
+        this.professionalTab = this.getVisibleHeaderTabByHref(siteLocale.routes.professional);
+        this.supportTab = this.getVisibleHeaderTabByHref(siteLocale.routes.support);
+    }
 
-        this.professionalTab = this.root
-            .locator(`a[href="${siteLocale.routes.professional}"]`)
-            .filter({ visible: true });
-
-        this.supportTab = this.root
-            .locator(`a[href="${siteLocale.routes.support}"]`)
+    private getVisibleHeaderTabByHref(href: string): Locator {
+        return this.root
+            .locator(`a.GQwn[href="${href}"]`)
             .filter({ visible: true });
     }
 
